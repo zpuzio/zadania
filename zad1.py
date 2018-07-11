f86848e97c05388bd2c96db16832a27f186265f0
Zuzanna Puzio <zuz2295@gmail.com>
2018-07-11 12:01:02 +0200

    first commit

---
+++ zad1.py
@@ -0,0 +1,9 @@
+name = raw_input("Jak masz na imie: ")
+age = int(raw_input("Ile masz lat: "))
+y = (2018 - age) + 100
+print("Czesc " + name)
+print("W " + str(y) + " skonczysz 100 lat")
+number = int(raw_input("Podaj cyfre: "))
+for i in range(number):
+    print("W " + str(y) + " skonczysz 100 lat")
+
