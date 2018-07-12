da1d93a51549bd5de438019bde9aaf2c243373ce
Zuzanna Puzio <zuz2295@gmail.com>
2018-07-12 11:04:38 +0200

    poprawione zad14

--- zad14.py
+++ zad14.py
@@ -1,17 +1,23 @@
-def duplicate():
-    a = [1,1,3,3,3,6,7,7,8,9,9,9,9,9,12,15]
+def delete_duplicate(a):
+    print type(a)
     print "Pierwotna lista" + str(a)
     aa = set(a)
     print "Nowa lista " + str(aa)
+    return list(aa)
 
-wynik = duplicate()
-
-def duplicate_loop():
-    a = [1, 1, 3, 3, 3, 6, 7, 7, 8, 9, 9, 9, 9, 9, 12, 15]
+def delete_duplicate_loop(a):
     b = []
     for i in a:
         if i not in b:
             b.append(i)
     print b
+    return list(b)
 
-wynik1 = duplicate_loop()
+def main():
+    c = [1,1,3,3,3,6,7,7,8,9,9,9,9,9,12,15]
+    wynik = delete_duplicate(c)
+    print(type(wynik))
+    wynik1 = delete_duplicate_loop(c)
+    print(type(wynik1))
+
+main()
