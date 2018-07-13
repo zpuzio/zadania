5e88e6e54624a719f2ae6d343caa5e4f19dbf8fa
Zuzanna Puzio <zuz2295@gmail.com>
2018-07-13 08:42:59 +0200

    zad20 dodane binary search

--- zad20.py
+++ zad20.py
@@ -9,15 +9,21 @@ def element_search(lista, number):
 
 def binary_search(lista, number):
     lista = sorted(lista)
-    d = len(lista)/2
-    if a[d]>number:
-        lista = lista[:d]
-
-
-
+    if len(lista) == 0:
+        return False
+    else:
+        d = len(lista)/2
+        if lista[d] == number:
+            return True
+        else:
+            if number > lista[d]:
+                return binary_search(lista[d+1:],number)
+            else:
+                return binary_search(lista[:d],number)
 
 
 if __name__=="__main__":
     l = [1,3,2,1,5,6,9,11]
     n = int(raw_input("Podaj liczbe: "))
-    print element_search(l,n)
+    #print element_search(l,n)
+    print binary_search(l, n)
