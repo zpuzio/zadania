2c1e4c597e1784e6bd4d6f636791876f36ad1094
Zuzanna Puzio <zuz2295@gmail.com>
2018-07-16 15:42:57 +0200

    zad18 poprawione#1

--- zad18.py
+++ zad18.py
@@ -1,21 +1,24 @@
 import random
-
+import itertools
 def cows_bulls(userDigit,randDigit):
     cows = 0
     bulls = 0
-    randDigit = map(int, str(randDigit)) #zamieniam int na liste
-    userDigit = map(int, str(userDigit))
+    userDigit = map(int, str(userDigit))#zamieniam int na liste
     for i in range(len(randDigit)):
-        if randDigit[i] in userDigit and randDigit[i] != userDigit[i]:
-            bulls += 1
         if randDigit[i] == userDigit[i]:
             cows += 1
-
+        elif userDigit[i] in randDigit:
+            bulls += 1
     return {'cows': cows, 'bulls': bulls}
 
 if __name__=="__main__":
     print "Welcome to Cows and Bulls Game!"
-    rand = random.randint(1000, 9999)
+    rands = []
+    iterables = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+    for t in itertools.product(*iterables):
+        rands.append(t)
+    rand = list(random.choice(rands))
     resultValues = 0
     countCows = 0
     countBulls = 0
