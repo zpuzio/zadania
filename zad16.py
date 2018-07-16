228f516d02b17edf7a329d0eaf44e8442b5ff5fb
Zuzanna Puzio <zuz2295@gmail.com>
2018-07-16 14:45:15 +0200

    zad16 poprawione

--- zad16.py
+++ zad16.py
@@ -1,38 +1,37 @@
 import random
+import string
 
-a = []
-b = []
-c = []
+passWord = []
 
 def password_generator():
     opt = raw_input("Wygenerowac haslo silne czy slabe [s/w]: ")
     if opt == "s":
-
-        for i in range(1,5):
-            a.append(random.choice("abcdefghijkmnoprstuwxyz"))
-            result = "".join(a)
-
-        r1 = random.sample(xrange(100),3)
-
         for i in range(1,4):
-            b.append(random.choice("abcdefghijkmnoprstuwxyz"))
-            c.append(random.choice(":.,?!$*)(#@%&\[]"))
-            r2 = "".join(c)
-            r = "".join(b)
-            r=r.upper()
-
-        items = result + r + str(r1[1])+r2+str(r1[0])
-        print ''.join(random.sample(items, len(items))) #shuffle strings
+            lowerLetter = "".join(random.choice(string.ascii_lowercase))
+            upperLetter = "".join(random.choice(string.ascii_uppercase))
+            digit = "".join(random.choice(string.digits))
+            symbols = "".join(random.choice(":.,?!$*)(#@%&\[]"))
+            passWord.append(lowerLetter + upperLetter + digit +symbols)
+            items = ''.join(passWord)
+        print ''.join(random.sample(items, len(items)))
+        return ''.join(random.sample(items, len(items)))
     else:
-        word = ["rzym","kot","klon","krowa","mysz","rower","szkola","lis"]
-        wordd = ["mama", "komputer", "jesion", "morze", "kubek", "karta", "olaf", "lisc"]
+        word = ["rzym","kot","klon","krowa","mysz","rower","szkola","lis", "mama", "komputer",
+                "jesion", "morze", "kubek", "karta", "olaf", "lisc"]
         number = ["23","88","19","70","12","65"]
-        w1 = random.choice(word)
-        w2 = random.choice(wordd)
-        n1 = random.choice(number)
-        print w1+w2.upper()+n1
+        wordUpper = random.choice(word)
+        wordLower = random.choice(word)
+        randNumber = random.choice(number)
+        if wordLower == wordUpper:
+            wordUpper = random.choice(word)
+            wordLower = random.choice(word)
+            print wordUpper.upper() + wordLower + randNumber
+            return wordUpper.upper() + wordLower + randNumber
+        else:
+            print wordUpper.upper() + wordLower + randNumber
+            return wordUpper.upper() + wordLower + randNumber
 
 def main():
-    password = password_generator()
+    password_generator()
 
 main()
