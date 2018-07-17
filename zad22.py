ff7e57e40a96a0fedfe07fd93a2900001d7e480e
Zuzanna Puzio <zuz2295@gmail.com>
2018-07-17 14:45:07 +0200

    zad22 poprawione

--- zad22.py
+++ zad22.py
@@ -1,30 +1,21 @@
 from collections import Counter
 
-
 file1 = open("name.txt", "r")
 wordcount1 = Counter(file1.read().split())
 print wordcount1
 
-#for pair in wordcount.items():
-    #print(pair)
 for x,y in wordcount1.items():
     print x,y
 
 #---------------------------------------------------------------
-
+print "---"*50
 word = []
+category = []
 lines = [line.rstrip('\n') for line in open('this.txt')]
 r = range(0,len(lines))
 
-with open("category.txt", "w") as open_file:
-    for i in r:
-        word.append(lines[i].split("/"))
-        open_file.write(word[i][2])
-        open_file.write("\n")
+for i in r:
+    word.append(lines[i].split("/"))
+    category.append(word[i][2])
+print Counter(category)
 
-file = open("category.txt", "r")
-wordcount = Counter(file.read().split())
-# print wordcount
-
-for x,y in wordcount.items():
-    print x,y
