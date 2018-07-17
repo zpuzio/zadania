aa92d9266395bcd66302a3cd098ca84942806822
Zuzanna Puzio <zuz2295@gmail.com>
2018-07-17 14:47:05 +0200

    zad25 poprawione

--- zad25.py
+++ zad25.py
@@ -1,20 +1,20 @@
-def binary_search(lista, number,c):
-    if len(lista) == 0:
-        return False
+def binary_search(lista,c):
+    if len(lista) == 1:
+        print "Podana przez Ciebie liczba to: " + str(lista[0])
     else:
         d = len(lista) / 2
         c += 1
         odp = raw_input("Czy liczba " + str(lista[d]) + " jest za wysoka, za mala czy dobra [d/m/ok]:  ")
         if odp == "m":
-            return binary_search(lista[d + 1:], number, c)
+            return binary_search(lista[d + 1:], c)
         elif odp == "d":
-            return binary_search(lista[:d], number, c)
+            return binary_search(lista[:d], c)
         else:
             print "Podana przez Ciebie liczba to: " + str(lista[d])
             print "Liczba zgadniec: " + str(c)
 
 if __name__=="__main__":
-    liczba = int(raw_input("Podaj liczbe od 0 do 100 ktora mam zgadnac: "))
+    print("Program ma zgadnac liczbe od 0 do 100")
     lista = range(0,101)
     c = 0
-    binary_search(lista,liczba,c)
+    binary_search(lista,c)
